ffeb61716920f292245dd5ef3726077895abccd0
GabrielMatosNogueira <gabrielmatosnogueiracompsci@gmail.com>
2024-11-28 19:44:22 -0300

    Nova Branch com algumas structs

---
+++ include/ingrediente.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int Id;
+    char Nome[30];
+    float Preço;
+} Ingredientes;
---
+++ include/pizza.h
@@ -0,0 +1,5 @@
+typedef struct {
+    int Id;
+    char Nome[30];
+    float Preço;
+} Ingredientes;
